ec3bfed9582856052c10ca2e3c300a01ee878e8a
bhargav-tibadiya <bhargavptibadiya.tbp@gmail.com>
2025-03-01 20:01:16 +0530

    Swagger docs optimized

--- src/utils/swagger/index.ts
+++ src/utils/swagger/index.ts
@@ -1,28 +1,10 @@
-import swaggerJSDoc from "swagger-jsdoc";
 import swaggerUi from "swagger-ui-express";
 import { Application } from "express";
 import { ROUTES } from "@utils/constants/routes";
+import YAML from "yamljs";
 
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Academia API Documentation",
-      version: "1.0.0",
-      description: "Academia is Student Information System. Refined version of Uka Tarsadia University SIS.",
-    },
-    servers: [
-      {
-        url: "http://localhost:5000/",
-        description: "Local server",
-      },
-    ],
-  },
-  apis: ["./src/utils/swagger/docs.ts"],
-};
-
-const swaggerSpec = swaggerJSDoc(options);
+const swaggerDocument = YAML.load("./src/utils/swagger/docs.yaml");
 
 export const setupSwagger = (app: Application) => {
-  app.use(ROUTES.DOCS, swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.use(ROUTES.DOCS, swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 };
