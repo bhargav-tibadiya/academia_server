25814f61dd4210f6b75029238a22e2a48570c045
kasishs-dev <kasishs1311@gmail.com>
2025-03-04 13:50:51 +0530

    build controller class, fee and , timetable

---
+++ src/controllers/timetable.controller.ts
@@ -0,0 +1,232 @@
+// Packages
+import mongoose from "mongoose";
+import { RequestHandler } from "express";
+
+// Models
+import TimeTable from "@models/timetable.model";
+
+// Utils & Config
+import logger from "@services/logger";
+import { responseCreator } from "@utils/helpers";
+
+// Types & Constants
+import { STATUS } from "@utils/constants/status";
+import { MESSAGES } from "@utils/constants/message";
+import { ServerResponse } from "@interfaces/controllers";
+
+// Get All TimeTables & Get TimeTable By ID
+export const getTimeTables: RequestHandler = async (
+  req,
+  res: ServerResponse
+) => {
+  const { timetableId } = req.params;
+  try {
+    if (timetableId) {
+      if (!mongoose.Types.ObjectId.isValid(timetableId)) {
+        logger.warn(`Invalid ObjectId format: ${timetableId}`);
+        res
+          .status(STATUS.BAD_REQUEST)
+          .json(
+            responseCreator(
+              STATUS.BAD_REQUEST,
+              MESSAGES.INVALID_OBJECT_ID,
+              false
+            )
+          );
+        return;
+      }
+
+      const timetable = await TimeTable.findById(timetableId).populate(
+        "classId"
+      );
+      if (!timetable) {
+        logger.warn(`${MESSAGES.RESOURCE_NOT_FOUND}: ${timetableId}`);
+        res
+          .status(STATUS.NOT_FOUND)
+          .json(
+            responseCreator(
+              STATUS.NOT_FOUND,
+              MESSAGES.RESOURCE_NOT_FOUND,
+              false
+            )
+          );
+        return;
+      }
+
+      logger.info(`Fetched timetable record: ${timetableId}`);
+      res
+        .status(STATUS.OK)
+        .json(
+          responseCreator(STATUS.OK, MESSAGES.DATA_FETCHED, true, timetable)
+        );
+      return;
+    }
+
+    const timetables = await TimeTable.find().populate("classId");
+    logger.info("Fetched all timetable records");
+    res
+      .status(STATUS.OK)
+      .json(
+        responseCreator(STATUS.OK, MESSAGES.DATA_FETCHED, true, timetables)
+      );
+    return;
+  } catch (error: any) {
+    logger.error(`Error in getTimeTables: ${error.message}`);
+    res
+      .status(STATUS.INTERNAL_SERVER_ERROR)
+      .json(
+        responseCreator(
+          STATUS.INTERNAL_SERVER_ERROR,
+          MESSAGES.INTERNAL_SERVER_ERROR,
+          false
+        )
+      );
+    return;
+  }
+};
+
+// Create a TimeTable Record
+export const createTimeTable: RequestHandler = async (
+  req,
+  res: ServerResponse
+) => {
+  const { classId, timetable } = req.body;
+  try {
+    const newTimeTable = new TimeTable({ classId, timetable });
+    await newTimeTable.save();
+    logger.info("Created a new timetable record");
+    res
+      .status(STATUS.CREATED)
+      .json(
+        responseCreator(
+          STATUS.CREATED,
+          MESSAGES.DATA_CREATED,
+          true,
+          newTimeTable
+        )
+      );
+    return;
+  } catch (error: any) {
+    logger.error(`Error in createTimeTable: ${error.message}`);
+    res
+      .status(STATUS.INTERNAL_SERVER_ERROR)
+      .json(
+        responseCreator(
+          STATUS.INTERNAL_SERVER_ERROR,
+          MESSAGES.INTERNAL_SERVER_ERROR,
+          false
+        )
+      );
+    return;
+  }
+};
+
+// Update a TimeTable Record
+export const updateTimeTable: RequestHandler = async (
+  req,
+  res: ServerResponse
+) => {
+  const { timetableId } = req.params;
+  const updateData = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(timetableId)) {
+    logger.warn(`Invalid ObjectId format: ${timetableId}`);
+    res
+      .status(STATUS.BAD_REQUEST)
+      .json(
+        responseCreator(STATUS.BAD_REQUEST, MESSAGES.INVALID_OBJECT_ID, false)
+      );
+    return;
+  }
+
+  try {
+    const updatedTimeTable = await TimeTable.findByIdAndUpdate(
+      timetableId,
+      updateData,
+      { new: true }
+    );
+    if (!updatedTimeTable) {
+      logger.warn(`${MESSAGES.RESOURCE_NOT_FOUND}: ${timetableId}`);
+      res
+        .status(STATUS.NOT_FOUND)
+        .json(
+          responseCreator(STATUS.NOT_FOUND, MESSAGES.RESOURCE_NOT_FOUND, false)
+        );
+      return;
+    }
+
+    logger.info(`Updated timetable record: ${timetableId}`);
+    res
+      .status(STATUS.OK)
+      .json(
+        responseCreator(
+          STATUS.OK,
+          MESSAGES.DATA_UPDATED,
+          true,
+          updatedTimeTable
+        )
+      );
+    return;
+  } catch (error: any) {
+    logger.error(`Error in updateTimeTable: ${error.message}`);
+    res
+      .status(STATUS.INTERNAL_SERVER_ERROR)
+      .json(
+        responseCreator(
+          STATUS.INTERNAL_SERVER_ERROR,
+          MESSAGES.INTERNAL_SERVER_ERROR,
+          false
+        )
+      );
+    return;
+  }
+};
+
+// Delete a TimeTable Record
+export const deleteTimeTable: RequestHandler = async (
+  req,
+  res: ServerResponse
+) => {
+  const { timetableId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(timetableId)) {
+    logger.warn(`Invalid ObjectId format: ${timetableId}`);
+    res
+      .status(STATUS.BAD_REQUEST)
+      .json(
+        responseCreator(STATUS.BAD_REQUEST, MESSAGES.INVALID_OBJECT_ID, false)
+      );
+    return;
+  }
+
+  try {
+    const deletedTimeTable = await TimeTable.findByIdAndDelete(timetableId);
+    if (!deletedTimeTable) {
+      logger.warn(`${MESSAGES.RESOURCE_NOT_FOUND}: ${timetableId}`);
+      res
+        .status(STATUS.NOT_FOUND)
+        .json(
+          responseCreator(STATUS.NOT_FOUND, MESSAGES.RESOURCE_NOT_FOUND, false)
+        );
+      return;
+    }
+
+    logger.info(`Deleted timetable record: ${timetableId}`);
+    res
+      .status(STATUS.OK)
+      .json(responseCreator(STATUS.OK, MESSAGES.DATA_DELETED, true));
+    return;
+  } catch (error: any) {
+    logger.error(`Error in deleteTimeTable: ${error.message}`);
+    res
+      .status(STATUS.INTERNAL_SERVER_ERROR)
+      .json(
+        responseCreator(
+          STATUS.INTERNAL_SERVER_ERROR,
+          MESSAGES.INTERNAL_SERVER_ERROR,
+          false
+        )
+      );
+    return;
+  }
+};
